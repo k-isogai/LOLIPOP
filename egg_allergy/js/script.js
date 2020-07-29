44a72629255bab6206f8f75c1da19728aee6b195
磯貝 健二 <k_isogai@wan55.co.jp>
2020-07-29 16:58:27 +0900

    ソート機能導入

--- egg_allergy/js/script.js
+++ egg_allergy/js/script.js
@@ -9,30 +9,41 @@ $(function () {
 	.done((data) => {
 
 		var table = document.createElement('table');
+		var thead = document.createElement("thead");
+		var tbody = document.createElement('tbody');
 		for (var i = 0; i < data.length; i++) {
 			var tr = document.createElement('tr');
-			// th・td部分のループ
-			for (var j = 0; j < Object.keys(data[i]).length; j++) {
-				var key = Object.keys(data[i])[j];
-				if(i === 0) {
+			if(i === 0) {
+				for (var j = 0; j < Object.keys(data[i]).length; j++) {
 					var th = document.createElement('th');
+					var key = Object.keys(data[i])[j];
 					th.textContent = data[i][key];
 					tr.appendChild(th);
-				} else {
+				}
+				thead.appendChild(tr);
+			} else {
+				for (var j = 0; j < Object.keys(data[i]).length; j++) {
 					var td = document.createElement('td');
+					var key = Object.keys(data[i])[j];
 					td.textContent = data[i][key];
 					tr.appendChild(td);
 				}
+				tbody.appendChild(tr);
 			}
-			table.appendChild(tr);
 		}
+		table.appendChild(thead);
+		table.appendChild(tbody);
 		document.getElementById('table').appendChild(table);
-
 	})
 	.fail((data) => {
 		console.log('JSON取得に失敗しました');
 	});
 
+	//ソート機能
+	setTimeout(function(){
+		$('#table table').tablesorter();
+	},500);
+	
 	// リアルタイム検索
 	$('#search-text').on('input', function(){
 
@@ -41,7 +52,7 @@ $(function () {
 		var check = new Array();
 		console.log(searchText);
 
-		$('#table tr:not(:first-child)').each(function() {
+		$('#table tbody tr').each(function() {
 			targetText = $(this).find('td').text();
 			console.log(targetText);
 
